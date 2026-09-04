75d61789cf206df3723220aa0bcf62283ab1fbc2
john105278-star <john105278@gmail.com>
2026-09-04 13:26:18 +0900

    Make v1.4 package identity permanent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.marshall.stockai.v13"
+        applicationId = "com.marshall.stockai.v14"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.3.0"
+        versionCode = 14
+        versionName = "1.4.0"
     }
 
     compileOptions {
